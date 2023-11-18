764aa02760837959d3cc7973b97a5363dc5f086a
barra-white <117594210+barra-white@users.noreply.github.com>
2023-11-18 18:54:27 +0000

    colour class test if not null

--- src/main/java/colourtablemanagement/Colour.java
+++ src/main/java/colourtablemanagement/Colour.java
@@ -1,4 +1,25 @@
 package colourtablemanagement;
 
 public class Colour {
+    // variables for red, green and blue values
+    private int red;
+    private int green;
+    private int blue;
+
+    // constructor
+    public Colour(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+    // get methods for each of the RGB values
+    public int getRed() {
+        return this.red;
+    }
+    public int getGreen() {
+        return this.green;
+    }
+    public int getBlue() {
+        return this.blue;
+    }
 }
--- src/test/java/colourtablemanagement/ColourTest.java
+++ src/test/java/colourtablemanagement/ColourTest.java
@@ -1,5 +1,14 @@
 package colourtablemanagement;
 
-public class ColourTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ColourTest {
+    @Test
+    // check if object is null after construction
+    void checkIfNull() {
+        Colour testColour = new Colour(255, 0, 0);
+        assertNotNull(testColour);
+    }
 }
