134734c7b9bbd7f109e56bfe8f9b8bef049bf4e4
barra-white <117594210+barra-white@users.noreply.github.com>
2023-11-18 18:58:52 +0000

    colour class test to check if assigning RGB values correctly

--- src/test/java/colourtablemanagement/ColourTest.java
+++ src/test/java/colourtablemanagement/ColourTest.java
@@ -11,4 +11,13 @@ public class ColourTest {
         Colour testColour = new Colour(255, 0, 0);
         assertNotNull(testColour);
     }
+
+    @Test
+    // check if constructor is assigning values correctly
+    void checkIfCorrectValues() {
+        Colour testColour = new Colour(147, 59, 72);
+        assertEquals(147, testColour.getRed());
+        assertEquals(59, testColour.getGreen());
+        assertEquals(72, testColour.getBlue());
+    }
 }
