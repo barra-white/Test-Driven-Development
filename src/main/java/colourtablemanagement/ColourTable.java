286cc59ea3fd9f92c95b6f543687be35750b84d5
barra-white <117594210+barra-white@users.noreply.github.com>
2023-11-18 20:51:34 +0000

    added methods + tests to verify if palette size meets requirements (1 < size < 1025) && (size is a power of 2)

--- src/main/java/colourtablemanagement/ColourTable.java
+++ src/main/java/colourtablemanagement/ColourTable.java
@@ -6,7 +6,26 @@ public class ColourTable {
 
     // constructor
     public ColourTable(int paletteSize) {
-        colourPalette = new Colour[paletteSize];
+        colourPalette = new Colour[this.validatePaletteSize(paletteSize)];
     }
 
+    // getter method to return palette size
+    public int getPaletteSize() {
+        return this.colourPalette.length;
+    }
+
+    // method to validate palette size
+    public int validatePaletteSize(int size) {
+        if (isPowerOf2(size) && size > 1 && size < 1025) {
+            return size;
+        }
+        else {
+            throw new IllegalArgumentException("Palette size must be between 2 and 1024, and must be a power of 2");
+        }
+    }
+
+    // method to check if a value is to the power of 2
+    public boolean isPowerOf2(int size) {
+        return (size & (size - 1)) == 0;
+    }
 }
--- src/test/java/colourtablemanagement/ColourTableTest.java
+++ src/test/java/colourtablemanagement/ColourTableTest.java
@@ -4,10 +4,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTableTest {
+
     @Test
-        // check if object is null after construction
+    // check if object is not null after intialisation
     void checkIfNull() {
-        ColourTable testTable = new ColourTable(10);
+        ColourTable testTable = new ColourTable(8);
         assertNotNull(testTable);
     }
+
+    @Test
+    // check if colour table size meets requirements (is a power of 2 and less than 1025)
+    void checkIfCorrectPaletteSize () {
+        ColourTable test1 = new ColourTable(2);
+        ColourTable test2 = new ColourTable(8);
+
+        // checks to see if size satisfies requirements array of that size is created
+        assertEquals(2, test1.getPaletteSize());
+        assertEquals(8, test2.getPaletteSize());
+
+        // checks if exception is thrown for invalid palette sizes
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(0));
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(10000));
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(7));
+    }
+
 }
